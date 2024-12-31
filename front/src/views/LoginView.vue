b9579b1f7cbb98e7fdf950b2b61d44744049780c
Lohan Alexandre <lohanalexandre@dpc.com.br>
2024-12-30 22:55:01 -0300

    Ambiente configurado e prototipo de tela de login
    Ambiente configurado e prototipo de tela de login

---
+++ front/src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <div class="flex flex-col items-center justify-center border-2 w-96 h-96 p-6">
+      <div class="flex flex-col items-center justify-center mb-6">
+        <h1 class="font-bold">TrelloLike</h1>
+      </div>
+      <h3>Entre com seu e-mail para realizar o login</h3>
+      <input
+        type="text"
+        name=""
+        id=""
+        class="border rounded-sm p-2 my-2 w-64"
+        placeholder="Insira seu e-mail"
+      />
+      <button class="bg-blue-500 text-white p-2 mt-6 rounded-sm w-32">Login</button>
+      <a href="" class="mt-6">Não possui cadastro? Crie uma aqui!</a>
+    </div>
+  </div>
+</template>
